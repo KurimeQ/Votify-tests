a4a6b5dbc4994cae66dd9062d94a93c75c118bcd
KurimeQ <m.kurimsky@email.cz>
2024-06-19 12:10:35 +0200

    Improving test

--- tests/onboarding.spec.ts
+++ tests/onboarding.spec.ts
@@ -3,28 +3,64 @@ import { LoginPage } from '../page-objects/LoginPage';
 import { OnboardingPage } from '../page-objects/OnboardingPage';
 
 test('Verify onboarding page title', async ({ page }) => {
+  test.info().annotations.push({
+    type: 'Test',
+    description: 'This test verifies the onboarding page title is visible after login.'
+  })
   const loginPage = new LoginPage(page);
   const onboardingPage = new OnboardingPage(page);
-  await loginPage.gotoLoginPage();
-  await loginPage.login();
-  await expect(onboardingPage.title).toBeVisible();
+  await test.step('Go to login page', async () => {
+    await loginPage.gotoLoginPage();
+  });
+  await test.step('Click login button', async () => {
+    await loginPage.login();
+  });
+  await test.step('Verify onboarding is visible', async () => {
+    await expect(onboardingPage.title, 'Onboarding was not shown or there is invalid link').toBeVisible();
+  });
 });
 
 test('Verify onboarding page texts', async ({ page }) => {
+  test.info().annotations.push({
+    type: 'Test',
+    description: 'This test verifies the onboarding page texts are visible and contain expected content.'
+  })
   const loginPage = new LoginPage(page);
   const onboardingPage = new OnboardingPage(page);
-  await loginPage.gotoLoginPage();
-  await loginPage.login();
-  await expect(onboardingPage.onboardingTitle).toBeVisible();
-  await expect(onboardingPage.onboardingText).toBeVisible();
+  await test.step('Go to login page', async () => {
+    await loginPage.gotoLoginPage();
+  });
+  await test.step('Click login button', async () => {
+    await loginPage.login();
+  });
+  await test.step('Verify onboarding Title is visible', async () => {
+    await expect(onboardingPage.onboardingTitle, 'Onboarding Title was not shown').toBeVisible();
+  });
+  await test.step('Verify onboarding Text is visible', async () => {
+    await expect(onboardingPage.onboardingText, 'Onboarding Text was not shown').toBeVisible();
+  });
 });
 
-test('Lets get started onboarding', async ({ page }) => {
+test.slow('Lets get started onboarding', async ({ page }) => {
+  test.info().annotations.push({
+    type: 'Test',
+    description: 'This test verifies clicking "Lets get started" button navigates to the next screen.'
+  })
   const loginPage = new LoginPage(page);
   const onboardingPage = new OnboardingPage(page);
-  await loginPage.gotoLoginPage();
-  await loginPage.login()
-  await expect(page).toHaveURL('https://app.votify.app/cs/onboarding?');
-  await onboardingPage.clickOnLetsGetStarted();
-  await expect(onboardingPage.profilHeader).toBeVisible()
+  await test.step('Go to login page', async () => {
+    await loginPage.gotoLoginPage();
+  });
+  await test.step('Click login button', async () => {
+    await loginPage.login();
+  });
+  await test.step('Verify that correct link was presented', async () => {
+    await expect(page, 'Onboarding was not shown or there is invalid link').toHaveURL('https://app.votify.app/cs/onboarding?');
+  });  
+  await test.step('Click Lets get started', async () => {
+    await onboardingPage.clickOnLetsGetStarted();
+  });
+  await test.step('Profil header is visible', async () => {
+    await expect(onboardingPage.profilHeader, 'Profil header is not visible').toBeVisible();
+  });
 });
